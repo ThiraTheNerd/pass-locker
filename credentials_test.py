fe55bc4e5c55c14237415d0e4b92790d43fcd950
ThiraTheNerd <thiragithinji@gmail.com>
2021-05-30 17:09:09 +0300

    add the find account test function

--- credentials_test.py
+++ credentials_test.py
@@ -10,7 +10,7 @@ class TestCredentials(unittest.TestCase):
     self.assertEqual(self.twitter_account.password, "twitter_pass")
   def test_save_credentials(self):
     self.twitter_account.save_credentials()
-    self.assertEqual(len(Credentials.user_accounts), 1)
+    self.assertEqual(len(Credentials.user_accounts), 2)
 
   def test_display_credentials(self):
     self.assertEqual(Credentials.display_credentials(), Credentials.user_accounts)
@@ -19,6 +19,11 @@ class TestCredentials(unittest.TestCase):
     self.twitter_account.save_credentials()
     self.twitter_account.delete_account_credentials()
     self.assertEqual(len(Credentials.user_accounts), 0)
+
+  def test_find_account(self):
+    self.twitter_account.save_credentials()
+    found_account = Credentials.find_account("twitter")
+    self.assertEqual(found_account.user_name, "twitter_username")
 
 
 
