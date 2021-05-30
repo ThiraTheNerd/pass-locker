09ff0576868ed72f27e95633b348a60ebe9a18a1
ThiraTheNerd <thiragithinji@gmail.com>
2021-05-30 09:07:43 +0300

    define the first function of creating a user

--- user.py
+++ user.py
@@ -1,4 +1,8 @@
 class User():
+  users_list = []
   def __init__(self,login_name,login_password):
     self.login_name = login_name
     self.login_password = login_password
+
+  def create_user(self):
+    User.users_list.append(self)
--- user_test.py
+++ user_test.py
@@ -9,5 +9,9 @@ class TestUser(unittest.TestCase):
     self.assertEqual(self.user_account.login_name, "user_name")
     self.assertEqual(self.user_account.login_password, "login_password")
 
+  def test_create_user(self):
+    self.user_account.create_user()
+    self.assertEqual(len(User.users_list), 1)
+
 if __name__ == '__main__':
   unittest.main()
