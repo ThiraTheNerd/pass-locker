83799102515d0eab08a566aacb17ba3938db3e97
ThiraTheNerd <thiragithinji@gmail.com>
2021-05-30 09:12:30 +0300

    save or store existing credentials function

--- credentials.py
+++ credentials.py
@@ -1,5 +1,10 @@
 class Credentials():
+
+  user_accounts= []
   def __init__(self,account_name,user_name,password):
     self.account_name = account_name
     self.user_name = user_name
     self.password = password
+
+  def save_credentials(self):
+    Credentials.user_accounts.append(self)
--- credentials_test.py
+++ credentials_test.py
@@ -8,6 +8,9 @@ class TestCredentials(unittest.TestCase):
     self.assertEqual(self.twitter_account.account_name, "twitter")
     self.assertEqual(self.twitter_account.user_name, "twitter_username")
     self.assertEqual(self.twitter_account.password, "twitter_pass")
+  def test_save_credentials(self):
+    self.twitter_account.save_credentials()
+    self.assertEqual(len(Credentials.user_accounts), 1)
 
 
 if __name__ == "__main__":
