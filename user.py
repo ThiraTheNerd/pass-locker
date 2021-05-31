b1885e3059ccb379f05e26dc4890a2b39258a389
ThiraTheNerd <thiragithinji@gmail.com>
2021-05-31 11:27:39 +0300

    add the function to verify a user login

--- user.py
+++ user.py
@@ -1,17 +1,24 @@
 class User():
   users_list = []
-  def __init__(self,login_name,login_password, login_data):
+  def __init__(self,login_name,login_password):
     self.login_name = login_name
     self.login_password = login_password
-    self.login_data = login_name + login_password
+  def create_user(self):
+    '''
+    Function to create a new user
+    '''
+    new_user = User(login_name, login_password)
 
   def save_user(self):
     User.users_list.append(self)
 
   @classmethod
-  def verify_login(cls,string):
+  def display_users(cls):
+    return cls.users_list
+
+  @classmethod
+  def verify_login(cls,logname,login_pass):
     for user in cls.users_list:
-      if user.login_data == string:
+      if (user.login_name == logname and user.login_password == login_pass):
         return True
       return False
-      
