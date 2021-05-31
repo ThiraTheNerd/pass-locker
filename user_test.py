39fc1f55ae19ccf795b071711bf0a6c95e934dae
ThiraTheNerd <thiragithinji@gmail.com>
2021-05-31 11:27:01 +0300

    test verrify login function

--- user_test.py
+++ user_test.py
@@ -4,7 +4,7 @@ from user import User
 class TestUser(unittest.TestCase):
 
   def setUp(self):
-    self.user_account = User("user_name", "login_password","")
+    self.user_account = User("user_name", "login_password")
   def test_init(self):
     self.assertEqual(self.user_account.login_name, "user_name")
     self.assertEqual(self.user_account.login_password, "login_password")
@@ -15,7 +15,7 @@ class TestUser(unittest.TestCase):
 
   def test_verify_user(self):
     self.user_account.save_user()
-    account_exists = User.verify_login("user_namelogin_password")
+    account_exists = User.verify_login("user_name", "login_password")
     self.assertTrue(account_exists)
 
 if __name__ == '__main__':
