80fb7a3bf53fbc8dc7d6d4561f5cebfd21157dab
ThiraTheNerd <thiragithinji@gmail.com>
2021-05-31 14:04:36 +0300

    add the introduction function

--- run.py
+++ run.py
@@ -2,6 +2,16 @@
 
 from user import User
 from credentials import Credentials
+import pyperclip
+
+def function():
+	print("               ____      __        ____     ____         __           ____      _____   _    __             ")
+	print("              |  _ \    /  \      / ___|   / ____|      |  |       /     \   /  _____| | |  / /          ")
+	print("              | |_) )  / __ \    / /___   / /____       |  |      |       | |  |       | | / /             ")
+	print("              |  __/  / |__| \   \____  \ \____ \       |  |      |  |_|  | |  |       | | \ \            ")
+	print("              | |    /   __   \   ____| |  ____| |      |  |_____ |       | |  |_____  | |  \ \           ")
+	print("              |_|   /___|  |___\ |_____ / |______/      |________| \_____/   \______ | |_|   \_\       ")
+function()
 
 def create_user(login_name, login_password):
   new_user = User(login_name, login_password)
@@ -40,6 +50,7 @@ def generate_password(number):
   '''
   auto_password = Credentials.generate_password(number)
   return auto_password
+
 
 
 def main():
@@ -82,10 +93,13 @@ def main():
     print(f"Hello {login_name} your account has been created successfully. Your new password is {login_pass}")
   while True:
     print("Use these short codes :\
+      lg - To Login once again\
       cc - Add new account credentials,\
       dc - display all credentials,\
       fc - find account\
       gp - generate a random password\
+      c  - To copy a string to the clipboard \
+      p  - To paste whaterver is copied in the clipboard\
       del - Delete a credential account,\
       ex -exit ")
     short_code = input().lower()
@@ -109,17 +123,40 @@ def main():
         elif pass_type == "gp":
           print("\n")
           print("Enter desired password length")
+          print("\n")
           string_length = int(input("Password Length: "))
           pass_word = generate_password(string_length)
+          print("\n")
           break
         else:
           print("\n")
           print("Invalid password")
+          print("\n")
           break
       save_account(create_account(acc_name,user_name,pass_word))
       print("\n")
       print(f"Account Credential for: {acc_name} - UserName: {user_name} - Password:{pass_word} created succesfully")
       print("\n")
+    elif short_code ==  "lg":
+      print("Enter your Login Credentials")
+      print("-"*20)
+      print("Username")
+      print("T-"*10)
+      logname = input("Username: ")
+      print("password: ")
+      print("-"*10)
+      pass_word = input()
+      while True:
+
+        if verify_user(logname, pass_word):
+          print("You have successfully Logged into Password Locker.")
+          print("\n")
+          break
+        else :
+          print("Data provided does not match any exsisting accounts.")
+          print("\n")
+          break
+
     elif short_code == "dc":
       if display_credentials():
         print("Here is a list of all your account credentials")
@@ -132,12 +169,14 @@ def main():
       else:
         print("\n")
         print("You have not saved any account credentials")
+        print("\n")
     elif short_code == "fc":
       print("Enter the account name")
       account = input()
       found_account= find_account(account)
       print("\n")
       print(f"{found_account.account_name}, {found_account.user_name}, {found_account.password}")
+      print("\n")
 
     elif short_code == "gp":
       print("Enter your desired password length")
@@ -147,14 +186,39 @@ def main():
       pass_word = generate_password(string_length)
       print("\n")
       print(f"Your generated password is {pass_word}")
-          
+      print("\n")
+
+    elif short_code == "c":
+      print("What text would you like to copy to clipboard?")
+      string_to_copy = input()
+      copy_string = pyperclip.copy(string_to_copy)
+      print("\n")
+      print("Your Text has been copied to clip-board")
+      print("\n")
+
+    elif short_code == "p":
+      while True:
+        if copy_string != 0 :
+          print(pyperclip.paste())
+          break
+        else:
+          print("\n")
+          print("Nothing has been copied to clip board")
+          print("\n")
+
     elif short_code == "del":
       print("Which account would you like to delete?")
       print("\n")
-      del_account = input("Enter the account name ie. Instagram")
+      del_account = input("Enter the account name ie. Instagram : ")
       delete_account(find_account(del_account))
       print("\n")
       print(f"{del_account} has been deleted successfully")
+      print("\n")
+    elif short_code == 'ex':
+      print("Bye...Thank you for using Pass Locker")
+      break
+    else:
+      print("I really did not get that. Kindly use the short-codes")
   
 
 
