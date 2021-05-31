69693800e8be556f07a03fb47d159432b9c5b730
ThiraTheNerd <thiragithinji@gmail.com>
2021-05-31 14:19:14 +0300

    add docstring to the test functions

--- credentials_test.py
+++ credentials_test.py
@@ -3,19 +3,34 @@ from credentials import Credentials
 
 class TestCredentials(unittest.TestCase):
   def setUp(self):
+    '''
+    creates the first instance of the class
+    '''
     self.twitter_account = Credentials("twitter", "twitter_username", "twitter_pass")
   def test_init(self):
+    '''
+    Tests the initialization function
+    '''
     self.assertEqual(self.twitter_account.account_name, "twitter")
     self.assertEqual(self.twitter_account.user_name, "twitter_username")
     self.assertEqual(self.twitter_account.password, "twitter_pass")
   def test_save_credentials(self):
+    '''
+    Tests the save function
+    '''
     self.twitter_account.save_credentials()
     self.assertEqual(len(Credentials.user_accounts), 2)
 
   def test_display_credentials(self):
+    '''
+    Tests the display function
+    '''
     self.assertEqual(Credentials.display_credentials(), Credentials.user_accounts)
 
   def test_delete_account_credentials(self):
+    '''
+    Tests the delete function
+    '''
     self.twitter_account.save_credentials()
     self.twitter_account.delete_account_credentials()
     self.assertEqual(len(Credentials.user_accounts), 0)
--- user.py
+++ user.py
@@ -1,6 +1,9 @@
 class User():
   users_list = []
   def __init__(self,login_name,login_password):
+    '''
+    Initializes the first instance of a class
+    '''
     self.login_name = login_name
     self.login_password = login_password
   def create_user(self):
@@ -10,14 +13,23 @@ class User():
     new_user = User(login_name, login_password)
 
   def save_user(self):
+    '''
+    Saves a user instance
+    '''
     User.users_list.append(self)
 
   @classmethod
   def display_users(cls):
+    '''
+    Displays all users
+    '''
     return cls.users_list
 
   @classmethod
   def verify_login(cls,logname,login_pass):
+    '''
+    Loops through list of users and verifies provided password and username
+    '''
     for user in cls.users_list:
       if (user.login_name == logname and user.login_password == login_pass):
         return True
